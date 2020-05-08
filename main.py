0884ff22792b298d8243b30293412398a9687891
MagicPutin <Vanya0411>
2020-05-08 23:02:31 +0300

    row and bullshit version

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import sklearn.neighbors
 import sklearn.preprocessing
+from scipy.linalg.tests.test_fblas import accuracy
+import pytest
+from sklearn import svm
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 row_data = open('data/wine.data', 'r')
 row_data = row_data.read()
 
 # preparing data
 data = [[i for i in j.split(',')] for j in row_data.split('\n')]
-print(data)
 feature = [i[1:] for i in data]
 wine_class = [i[0] for i in data]
-print(feature)
-print(wine_class)
 
 # cross validation of data
+# generator of folding
 kf = sklearn.model_selection.KFold(n_splits=5, shuffle=True, random_state=42)
-print(sklearn.model_selection.cross_val_score(estimator=kf, X=feature, y=wine_class))
-"""for k in range(1,51):
-    sklearn.neighbors.KNeighborsClassifier(n_neighbors=k,)"""
+clf = svm.SVC(kernel='linear', C=1)
+# yeah, thats works, but I need to build it more automatically
+# so, yesterday, I'll write smth to automate this
+for k in range(1,51):
+    k_neighbors = sklearn.neighbors.KNeighborsClassifier(n_neighbors=k)
+    print(k, round(sklearn.model_selection.cross_val_score(estimator=k_neighbors, X=feature, y=wine_class, cv=kf).mean(), 2))
 
